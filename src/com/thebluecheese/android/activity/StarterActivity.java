a7a55a82912e705ce10b2337fe971fc02cef5049
Charlie Gao <gcte2010@gmail.com>
2014-08-28 12:53:24 -0500

    new ui

--- src/com/thebluecheese/android/activity/StarterActivity.java
+++ src/com/thebluecheese/android/activity/StarterActivity.java
@@ -8,12 +8,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class StarterActivity extends Activity {
 
 	private static final String TAG = "BlueCheese";
 	protected Button _button;
+	protected TextView _aboutUsText;
 
 
 	@Override
@@ -25,12 +27,21 @@ public class StarterActivity extends Activity {
 		/*=======================Test Ends================================*/		
 		_button = (Button) findViewById(R.id.button);
 		_button.setOnClickListener(new ButtonClickHandler());
+		_aboutUsText = (TextView) findViewById(R.id.aboutUsText);
+		_aboutUsText.setOnClickListener(new TextClickHandler());
 	}
 
 	public class ButtonClickHandler implements View.OnClickListener {		
 		public void onClick(View view) {
 			Log.v(TAG, "Starting Camera Result Intent");
 			Intent intent = new Intent(StarterActivity.this, CameraResultActivity.class);
+			startActivity(intent);
+		}
+	}
+	public class TextClickHandler implements View.OnClickListener {		
+		public void onClick(View view) {
+			Log.v(TAG, "Starting About us");
+			Intent intent = new Intent(StarterActivity.this, AboutusActivity.class);
 			startActivity(intent);
 		}
 	}
